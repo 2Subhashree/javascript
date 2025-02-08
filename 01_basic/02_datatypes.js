98f2f8a3393e6d07590dc30537dbb300d6edf4ef
Subhashree Behera <139705336+2Subhashree@users.noreply.github.com>
2025-02-08 10:15:17 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "subha"
+let age = 23
+let isLoggedIn = false
+let state = null
+let city;
+//number
+
+console.log(typeof "subha")
+console.log(typeof null);
+console.log(typeof undefined);
+
