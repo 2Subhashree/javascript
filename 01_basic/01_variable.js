1dcd4b01635d71aea41857d2ebe4de7f68437b99
Subhashree Behera <139705336+2Subhashree@users.noreply.github.com>
2025-02-08 09:14:53 +0000

    done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "subha@gmail.com"
+var accountPassword = "12345"
+accountCity = "BBSR"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "Su12@gmail.com"
+accountPassword = "2121"
+accountCity = "cuttack"
+
+console.log(accountId)
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
